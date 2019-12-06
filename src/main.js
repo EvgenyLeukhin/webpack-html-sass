381fefa9d66b855addde0a41be0b6ffa3ae421a8
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-12-06 23:22:40 +0600

    iziModal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './style.scss';
+import iziModal from 'izimodal/js/iziModal';
+
+// Initialise imported function as jQuery function
+$.fn.iziModal = iziModal;
 
 // jQUERY //
 $(document).ready(() => {
-  $('body').addClass('color');
+
+  // initialize modal
+  $("#modal-youtube").iziModal({
+    title: 'What is GRC?',
+    headerColor: 'black',
+    background: 'black',
+    borderBottom: false,
+    closeButton: true,
+    transitionIn: 'fadeInDown',
+    iframe: true,
+    iframeHeight: 315,
+    width: 560,
+    fullscreen: true,
+    iframeURL: "https://www.youtube.com/embed/F3TGcQWCH1g",
+    history: false,
+    loop: true
+  });
+
 });
